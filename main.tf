6793e8ed11cbe47c69858ba7f8649de642206214
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-20 11:40:18 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   version = "~>2.46.0"
+  skip_provider_registration = true
     features {}
     subscription_id="4ce31b1f-a57c-460a-8872-01fb7723db79"
     tenant_id="0c6fce42-0449-4574-bf9c-a261c5635a02"
