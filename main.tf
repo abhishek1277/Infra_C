ea527a5e08035358352133c5c5c7f7c2d4725b7d
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-21 11:30:56 +0530

    ds

--- main.tf
+++ main.tf
@@ -14,16 +14,6 @@ provider "azurerm" {
   }
 
 
-  module "windowsservers" {
-    source              = "Azure/vm/azurerm"
-    location            = "West US 2"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
-    vm_os_simple        = "WindowsServer"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
-  }
-
   module "vnet" {
     source              = "Azure/vnet/azurerm"
     version             = "~> 1.0.0"
