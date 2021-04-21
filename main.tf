c52090b1e632e374a598a77f82144633b8748415
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-21 13:07:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,14 +20,10 @@ resource "azurerm_resource_group" "main" {
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
 }
- module "subnet" {
-    source              = "./modules/subnet"
+ module "vnet" {
+    source              = "Azure/vnet/azurerm"
     location            = "WestEurope"
-    resource_group_name = "resources"
+    resource_group_name = azurerm_resource_group.main.name
+    address_space       = ["10.0.0.0/16"]
   }
