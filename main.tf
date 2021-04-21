2e70a268773001f1fb3b20e76f85e73d447d3985
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-21 14:44:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "vnet" {
-  source              = "Azure/vnet/azurerm"
+  source              = "modules/subnet"
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
