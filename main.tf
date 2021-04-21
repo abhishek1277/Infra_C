0a29cb871c63966e577b0651abde0f1ff651332c
abhishek1277 <1210316301@gitam.in>
2021-04-21 12:26:05 +0530

    s

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "Prod112"
+    workspaces = {
+      name = "Infra_C"
+    }
+  }
+}
+
 provider "azurerm" {
   version = "~>2.46.0"
     features {}
+  }
 
-}
 
-resource "azurerm_resource_group" "xlabsrg" {
-  name     = "r2"
-  location = "East US"
-}
+  module "vnet" {
+    source              = "Azure/vnet/azurerm"
+    version             = "~> 1.0.0"
+    location            = "West US 2"
+    resource_group_name = "terraform-vm"
+  }
