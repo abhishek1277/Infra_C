4953e5c29f06f5f069094c0e77ec030d8209869b
abhishek1277 <1210316301@gitam.in>
2021-04-21 14:56:02 +0530

    commitU

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
-module "vnet" {
-  source              = "./modules/main"
+module "subnet" {
+  source              = "./modules/subnet"
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
