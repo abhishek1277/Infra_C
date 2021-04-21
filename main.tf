bd2836741c4a7ec5e740b338c1f20ecec2c93b9c
abhishek1277 <1210316301@gitam.in>
2021-04-21 12:58:35 +0530

    commit

--- main.tf
+++ main.tf
@@ -14,9 +14,20 @@ provider "azurerm" {
   }
 
 
-  module "vnet" {
-    source              = "Azure/vnet/azurerm"
-    version             = "~> 1.0.0"
-    location            = "West US 2"
-    resource_group_name = "terraform-vm"
+ 
+resource "azurerm_resource_group" "main" {
+  name     = "resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+ module "subnet" {
+    source              = "./modules/subnet"
+    location            = "WestEurope"
+    resource_group_name = "resources"
   }
