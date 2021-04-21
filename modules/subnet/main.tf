04703bb4c95b36dbc083ab3fff9b27b89dc14f25
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-21 15:46:20 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "internal1"
   resource_group_name  = "my-resources"
   virtual_network_name = "network"
   address_prefixes     = ["10.0.2.0/24"]
