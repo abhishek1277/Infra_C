ceafa1223faf1c239395a21583f5ca0ac3439c70
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-22 11:08:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,23 @@
 variable "resource_group_name" {
     type        = string
     description = "RG name in Azure"
+    defult ="My_Terraform_RG"
 }
 
 variable "resource_group_location" {
     type        = string
     description = "RG location in Azure"
+    defult ="West Europe"
 }
 
 variable "app_service_plan_name" {
     type        = string
     description = "App Service Plan name in Azure"
+    defult ="my-appserviceplan"
 }
 
 variable "app_service_name" {
     type        = string
     description = "App Service name in Azure"
+    defult = "terraform-demo-009"
 }
