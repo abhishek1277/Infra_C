e76b81dbf035efcbec142417638d810c30632f97
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-22 11:02:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,23 +16,12 @@ provider "azurerm" {
 
  
 resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
-resource "azurerm_virtual_network" "main" {
-  name                = "network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-module "subnet" {
-  source              = "./modules/subnet"
- 
-}
-
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
+  name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -43,7 +32,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "app-service-demo"
+  name                = var.app_service_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
