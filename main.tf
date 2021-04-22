2e5a7812fba66402a20c62be05a3a020a4a7db10
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-22 10:20:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,6 @@ module "subnet" {
  
 }
 
-  resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
 
 resource "azurerm_app_service_plan" "example" {
   name                = "example-appserviceplan"
