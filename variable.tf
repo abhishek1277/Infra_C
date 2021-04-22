a019b9fc38d9b384775727550b6912989de76d25
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-22 11:09:35 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,23 +1,23 @@
 variable "resource_group_name" {
     type        = string
     description = "RG name in Azure"
-    defult ="My_Terraform_RG"
+    default ="My_Terraform_RG"
 }
 
 variable "resource_group_location" {
     type        = string
     description = "RG location in Azure"
-    defult ="West Europe"
+    default ="West Europe"
 }
 
 variable "app_service_plan_name" {
     type        = string
     description = "App Service Plan name in Azure"
-    defult ="my-appserviceplan"
+    default ="my-appserviceplan"
 }
 
 variable "app_service_name" {
     type        = string
     description = "App Service name in Azure"
-    defult = "terraform-demo-009"
+    default = "terraform-demo-009"
 }
