c0b1d6e8b3418054e29934b34928d8e8235cdaf8
Abhishek1277 <abhishekcse98@gmail.com>
2021-04-30 11:03:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,3 +58,32 @@ resource "azurerm_data_factory" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
+
+resource "azurerm_logic_app_workflow" "example" {
+  name                = "workflow1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_logic_app_action_custom" "example" {
+  name         = "example-action"
+  logic_app_id = azurerm_logic_app_workflow.example.id
+
+  body = <<BODY
+{
+    "description": "A variable to configure the auto expiration age in days. Configured in negative number. Default is -30 (30 days old).",
+    "inputs": {
+        "variables": [
+            {
+                "name": "ExpirationAgeInDays",
+                "type": "Integer",
+                "value": -30
+            }
+        ]
+    },
+    "runAfter": {},
+    "type": "InitializeVariable"
+}
+BODY
+
+}
