4730402041af83a60fa656fb80fcf02caf69e868
Abhishek1277 <abhishekcse98@gmail.com>
2021-07-21 21:56:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,18 +29,6 @@ resource "azurerm_app_service_plan" "example" {
     tier = "Standard"
     size = "S1"
   }
-}
-
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service234"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
 
   
 }
